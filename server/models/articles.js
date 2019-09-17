fc906143967bc7f56ac1f38fe97f84f5b83a2627
Patrick <tunezepatrick@gmail.com>
2019-09-17 21:19:27 +0200

    feature:add endpoint to allow user to view specific article #168373972

--- server/models/articles.js
+++ server/models/articles.js
@@ -6,6 +6,18 @@ const articles = [
     title: 'Today',
     article: 'Hello My Best Friends ! Today I Just Want Share With You This Nice Quote!:Self-Belief And Hard Work Will Always Earn You Success.',
     authorId: 1,
+    comments: [
+      {
+        commentId: 1,
+        authorId: 1,
+        comment: 'This is my comment on this post',
+      },
+      {
+        commentId: 2,
+        authorId: 1,
+        comment: 'This is my second comment on this post',
+      },
+    ],
   },
   {
     id: 2,
@@ -13,6 +25,18 @@ const articles = [
     title: 'Love this day',
     article: 'Hello My Best Friends ! Today I Just Want Share With You This Nice Quote!:Self-Belief And Hard Work Will Always Earn You Success.',
     authorId: 2,
+    comments: [
+      {
+        commentId: 1,
+        authorId: 2,
+        comment: 'I really love this post',
+      },
+      {
+        commentId: 2,
+        authorId: 1,
+        comment: 'My name is patrick and this is my comment on this post',
+      },
+    ],
   },
   {
     id: 3,
@@ -20,6 +44,18 @@ const articles = [
     title: 'Hey Guys!',
     article: 'Hello My Best Friends ! Today I Just Want Share With You This Nice Quote!:Self-Belief And Hard Work Will Always Earn You Success.',
     authorId: 3,
+    comments: [
+      {
+        commentId: 1,
+        authorId: 3,
+        comment: 'wow! this is a great post ever',
+      },
+      {
+        commentId: 2,
+        authorId: 1,
+        comment: 'My name is patrick, i like this post',
+      },
+    ],
   },
 ];
 
--- server/routes/articles.js
+++ server/routes/articles.js
@@ -4,6 +4,7 @@ const articles = require('../controllers/articles');
 const updatePostedArticle = require('../controllers/updatepostedarticle');
 const deleteArticle = require('../controllers/deleteArticle');
 const comments = require('../controllers/comments');
+const viewArticle = require('../controllers/viewArticle');
 const auth = require('../middleware/authorization');
 
 const router = express.Router();
@@ -12,4 +13,5 @@ router.post('/', auth, articles);
 router.patch('/:id/', auth, updatePostedArticle);
 router.delete('/:id/', auth, deleteArticle);
 router.post('/:id/comments/', auth, comments);
+router.get('/:id/', auth, viewArticle);
 module.exports = router;
