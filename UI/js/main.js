69bc6e99a2e590697ecc859869206a2eca348db6
Patrick <tunezepatrick@gmail.com>
2019-09-25 22:00:27 +0200

    bug(css):fix ui colors issue
    [Finishes #168760540]

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,40 +1,35 @@
-function toggleSignup(){
-    document.getElementById("login-toggle").style.backgroundColor="#fff";
-     document.getElementById("login-toggle").style.color="#222";
-     document.getElementById("signup-toggle").style.backgroundColor="#68af5b";
-     document.getElementById("signup-toggle").style.color="#fff";
-     document.getElementById("login-form").style.display="none";
-     document.getElementById("signup-form").style.display="block";
- }
+/* eslint-disable linebreak-style */
+function Signup() {
+  document.getElementById('login').style.backgroundColor = '#fff';
+  document.getElementById('login').style.color = '#1E1F26';
+  document.getElementById('signup').style.backgroundColor = '#ebebeb';
+  document.getElementById('signup').style.color = '#1E1F26';
+  document.getElementById('login-form').style.display = 'none';
+  document.getElementById('signup-form').style.display = 'block';
+}
  
- function toggleLogin(){
-     document.getElementById("login-toggle").style.backgroundColor="#68af5b";
-     document.getElementById("login-toggle").style.color="#fff";
-     document.getElementById("signup-toggle").style.backgroundColor="#fff";
-     document.getElementById("signup-toggle").style.color="#222";
-     document.getElementById("signup-form").style.display="none";
-     document.getElementById("login-form").style.display="block";
- }
+function Login() {
+  document.getElementById('login').style.backgroundColor = '#ebebeb';
+  document.getElementById('login').style.color = '#1E1F26';
+  document.getElementById('signup').style.backgroundColor = '#fff';
+  document.getElementById('signup').style.color = '#1E1F26';
+  document.getElementById('signup-form').style.display = 'none';
+  document.getElementById('login-form').style.display = 'block';
+}
  
 
- window.onload = function() {
+window.onload = function() {
+  /* side Bar */
+  document.getElementById('sidebar-btn')
+    .addEventListener('click', function() {
+      document.getElementById('sidebar')
+        .classList.toggle('visible');
+    });
+  /* Top Bar */
+	    document.getElementById('menu')
+	     .addEventListener('click',openMenu);
+};
 
-	/* side Bar */
-        document.getElementById('sidebar-btn')
-        .addEventListener("click", function() {
-          document.getElementById('sidebar')
-          .classList.toggle('visible');
-        });
-
-
-    /* Top Bar */
-	    document.getElementById("menu")
-	     .addEventListener("click",openMenu); 
-	        
-     };
-
-
-
-function openMenu(){
-	document.getElementById("dropdown").classList.toggle("active");
+function openMenu() {
+  document.getElementById('dropdown').classList.toggle('active');
 }
