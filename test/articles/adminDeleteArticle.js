39781f53289319d59ec9b3a5466de4f332c66cdc
Patrick <tunezepatrick@gmail.com>
2019-10-03 14:21:01 +0200

    chore(code-coverage):change the way of storing test coverage
    [Finishes #168895744]

--- test/articles/adminDeleteArticle.js
+++ test/articles/adminDeleteArticle.js
@@ -21,36 +21,4 @@ describe('When admin needs to delete inappropriate article ', () => {
         done();
       });
   });
-
-  const token = jwt.sign(mockData.userPayload, process.env.JWT_KEY, { expiresIn: '365d' });
-
-  // it('should not be able to delete inappropriate article if not an admin', (done) => {
-  //   chai.request(app)
-  //     .delete('/api/v1/articles/2')
-  //     .set('x-auth-token', token)
-  //     .send()
-  //     .end((err, res) => {
-  //       res.should.have.status(403);
-  //       res.should.be.an('object');
-  //       res.body.should.have.property('status').eql(403);
-  //       res.body.should.have.property('error');
-  //       done();
-  //     });
-  // });
-
-  const token2 = jwt.sign(mockData.adminPayload, process.env.JWT_KEY, { expiresIn: '365d' });
-
-  // it('should be able to delete inappropriate article if you are the admin', (done) => {
-  //   chai.request(app)
-  //     .delete('/api/v1/articles/2')
-  //     .set('x-auth-token', token2)
-  //     .send()
-  //     .end((err, res) => {
-  //       res.should.have.status(200);
-  //       res.should.be.an('object');
-  //       res.body.should.have.property('status').eql(200);
-  //       res.body.should.have.property('message');
-  //       done();
-  //     });
-  // });
 });
